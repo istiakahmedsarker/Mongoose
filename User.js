abc2257da1a602ddcd52cbdffba6e5f350df9207
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-18 21:18:36 +0600

    advanced schema created

--- User.js
+++ User.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     name: String,
     age: Number,
+    email: String,
+    createdAt: Date,
+    updatedAd: Date,
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+    hobbies: [String],
+    address:{
+        street: String,
+        city: String,
+    }
 })
 
 module.exports = mongoose.model("User",userSchema)
--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const User = require("./User")
 
 mongoose.connect("mongodb://localhost/testdb")
 
+run()
 async function run(){
-    const user = new User({name: "John", age: 26})
+    const user = await User.create({name: "John", age: 26})
+    user.name = "Sally"
     await user.save()
+    // const user = new User({name: "John", age: 26})
+    console.log(user)
 }
