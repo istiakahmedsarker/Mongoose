d7742b9bf9e1da37e53d4a49052eb2bb3c64b150
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-18 21:19:47 +0600

    send data in schema

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ const User = require("./User")
 mongoose.connect("mongodb://localhost/testdb")
 
 run()
-async function run(){
-    const user = await User.create({name: "John", age: 26})
+async function run() {
+    const user = await User.create(
+        {
+            name: "John",
+            age: 26,
+            hobbies:["Weight Lifting","Bowling"],
+            address:{
+                street:"Main St"
+            }
+        }
+    )
     user.name = "Sally"
     await user.save()
     // const user = new User({name: "John", age: 26})
