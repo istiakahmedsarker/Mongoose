433ad16ee70892ebf9c3ff74525a1f86c8433716
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-18 21:13:00 +0600

    saved data to the database

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose')
+const User = require("./User")
 
 mongoose.connect("mongodb://localhost/testdb")
+
+const user = new User({name: "John", age: 26})
+user.save().then(()=> console.log("USer Saved"))
